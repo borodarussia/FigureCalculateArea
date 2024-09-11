1484531b307678e69792ff5b8b16071cf9871a28
Ivan Kudryashov <boroda1337russia@gmail.com>
2024-09-11 23:28:33 +0400

    Мелкие исправления

--- Figure.Library/Services/FigureParametersCalculator.cs
+++ Figure.Library/Services/FigureParametersCalculator.cs
@@ -7,11 +7,36 @@ using System.Threading.Tasks;
 
 namespace Figure.Library.Services
 {
+    /// <summary>
+    /// Статический класс для расчета площади в режиме компиляции
+    /// </summary>
     public static class FigureParametersCalculator
     {
-        public static double CalculateSquare(IFigure figure)
+        /// <summary>
+        /// Расчет площади фигуры
+        /// </summary>
+        /// <param name="figure"></param>
+        /// <returns></returns>
+        public static double CalculateSquare<T>(T figure) where T : IFigure
         {
             return figure.CalculateSquare();
+        }
+
+        /// <summary>
+        /// Расчет списка фигур
+        /// </summary>
+        /// <param name="figures"></param>
+        /// <returns></returns>
+        public static IEnumerable<double> CalculateSquares<T>(IEnumerable<T> figures) where T : IFigure
+        {
+            List<double> squares = new List<double>();
+
+            foreach (IFigure f in figures)
+            {
+                squares.Add(CalculateSquare(f));
+            }
+
+            return squares;
         }
     }
 }
